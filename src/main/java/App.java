e80b0596974f709f0d5ad8095e9a18d2d22b45ff
Krystian Sosnowski <kr.sosnowski@gmail.com>
2019-08-17 21:38:27 +0200

    Dodano pętle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,37 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Zmiana");
+        System.out.println("Hello world!");
+
+        // 1.1 Pętla While
+        System.out.println("While p.1");
+        int value = 0;
+
+        while(value < 10)
+        {
+            System.out.println("hello " + value );
+            value = value + 1;
+        }
+
+        // 1.2 Pętla for
+        System.out.println("For p.1");
+        for(int i=0; i<5; i++)
+        {
+            System.out.printf("Hello I is: %d\n", i);
+        }
+
+        // 1.3 Pętla while
+        System.out.println("While p.2");
+        int loop=0;
+        while(true)
+        {
+            System.out.println("Looping "+ loop);
+
+            if(loop==5)
+            {
+                break;
+            }
+            loop++;
+        }
+
     }
 }
